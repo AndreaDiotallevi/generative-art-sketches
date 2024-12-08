57132b055392ff84a0aa53ac7c21dd528e13d9b0
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-12-08 23:42:08 +0200

    progress on sketch

--- src/scripts/ciatta/20241108.ts
+++ src/scripts/ciatta/20241108.ts
@@ -2,8 +2,8 @@ import p5 from "p5"
 import { canvasWidth, canvasHeight } from "@utils/globals"
 
 export const sketch = (p5: p5) => {
-    let t = 0
     const radius = canvasWidth / 20
+    const minRadius = radius / 7
     const circleSize = radius / 4
 
     const getRandomColour = () => {
@@ -34,21 +34,48 @@ export const sketch = (p5: p5) => {
                 p5.fill(getRandomColour())
                 p5.stroke(getRandomColour())
                 p5.strokeWeight(Math.ceil(p5.random(0, 3)))
-                drawOctagons({ x, y, r: radius * 2 })
+                drawShapes({ x, y, r: radius * 2, divisions: 8 })
+            }
+        }
+
+        for (
+            let x = (radius * 2.75) / 2;
+            x < canvasWidth + radius * 2;
+            x += radius * 2.75
+        ) {
+            for (
+                let y = (radius * 2.75) / 2;
+                y < canvasHeight + radius * 2;
+                y += radius * 2.75
+            ) {
+                p5.fill(getRandomColour())
+                p5.stroke(getRandomColour())
+                p5.strokeWeight(Math.ceil(p5.random(0, 3)))
+                drawShapes({ x, y, r: radius / 2, divisions: 4 })
             }
         }
     }
 
-    const drawOctagons = ({ x, y, r }: { x: number; y: number; r: number }) => {
+    const drawShapes = ({
+        x,
+        y,
+        r,
+        divisions,
+    }: {
+        x: number
+        y: number
+        r: number
+        divisions: number
+    }) => {
         // Guard condition: stop recursion when the radius is too small
-        if (r < 5) return
+        if (r < minRadius) return
 
         // Draw the current octagon
         if (p5.random() < 0.2) p5.fill(getRandomColour())
         if (p5.random() < 0.5) p5.stroke(getRandomColour())
         if (p5.random() < 0.2) p5.strokeWeight(Math.ceil(p5.random(0, 3)))
 
-        for (let angle = 0; angle < p5.TWO_PI; angle += p5.TWO_PI / 8) {
+        for (let angle = 0; angle < p5.TWO_PI; angle += p5.TWO_PI / divisions) {
             const childX = p5.cos(angle) * r + x
             const childY = p5.sin(angle) * r + y
 
@@ -56,7 +83,7 @@ export const sketch = (p5: p5) => {
             p5.circle(childX, childY, circleSize)
 
             // Recursively draw smaller octagons at each vertex
-            drawOctagons({ x: childX, y: childY, r: r * 0.25 })
+            drawShapes({ x: childX, y: childY, r: r * 0.25, divisions })
         }
     }
 
@@ -67,9 +94,9 @@ export const sketch = (p5: p5) => {
     p5.windowResized = () => {
         const newElement = document.querySelector("#sketch") as HTMLElement
         p5.resizeCanvas(newElement.clientWidth, newElement.clientHeight)
-        t = 0
+        // t = 0
         p5.draw()
     }
 }
 
-// try   drawOctagons({ x, y, r: radius * 3 })
+// try   drawShapes({ x, y, r: radius * 3 })
