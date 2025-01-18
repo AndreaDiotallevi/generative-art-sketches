a4c4b9004204ac4cdffe5f85a7f7619fdf4b7157
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2025-01-18 13:38:49 +0000

    do not save

--- src/scripts/ciatta/20250118.ts
+++ src/scripts/ciatta/20250118.ts
@@ -17,7 +17,7 @@ export const sketch = (p5: p5) => {
         p5.push()
         p5.background(
             p5.random() * 360,
-            p5.randomGaussian(0.5) * 100,
+            p5.randomGaussian(0.75) * 100,
             p5.randomGaussian(1) * 100,
             1
         )
@@ -25,7 +25,7 @@ export const sketch = (p5: p5) => {
         p5.translate(canvasWidth / 2, canvasHeight / 2)
 
         for (let i = 1; i < 3000; i += i / 2 + 1) {
-            p5.fill(p5.random() * 360, 100, 100, p5.randomGaussian(0.4, 0.35))
+            p5.fill(p5.random() * 360, 100, 100, p5.randomGaussian(0.4, 0.3))
 
             for (let j = 0; j < 2 * p5.PI; j += p5.PI / (p5.noise(i) * 36)) {
                 const x = p5.cos(j) * p5.noise(i) * i * ratio
@@ -37,23 +37,35 @@ export const sketch = (p5: p5) => {
     }
 
     p5.draw = () => {
-        for (let i = 0; i < 20; i++) {
-            randomSeed = i
-            noiseSeed = i * 1000
-            p5.randomSeed(randomSeed)
-            p5.noiseSeed(noiseSeed)
-            drawSketch()
-            console.log(randomSeed, noiseSeed)
-            // p5.save(
-            //     canvasWidth +
-            //         "-galassia-rSeed=" +
-            //         randomSeed +
-            //         "-nSeed=" +
-            //         noiseSeed +
-            //         ".png"
-            // )
-        }
+        randomSeed = Math.ceil(p5.random(10000))
+        // randomSeed = 19
+        noiseSeed = randomSeed * 1000
+        p5.randomSeed(randomSeed)
+        p5.noiseSeed(noiseSeed)
+        drawSketch()
+        // p5.text("Random seed = " + randomSeed, 20, 20)
+        // p5.text("Noise seed = " + noiseSeed, 20, 40)
+        console.log(randomSeed, noiseSeed)
     }
+
+    // p5.draw = () => {
+    //     for (let i = 0; i < 20; i++) {
+    //         randomSeed = i
+    //         noiseSeed = i * 1000
+    //         p5.randomSeed(randomSeed)
+    //         p5.noiseSeed(noiseSeed)
+    //         drawSketch()
+    //         console.log(randomSeed, noiseSeed)
+    //         // p5.save(
+    //         //     canvasWidth +
+    //         //         "-galassia-rSeed=" +
+    //         //         randomSeed +
+    //         //         "-nSeed=" +
+    //         //         noiseSeed +
+    //         //         ".png"
+    //         // )
+    //     }
+    // }
 
     p5.mouseClicked = () => {
         // p5.remove()
